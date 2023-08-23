e9806e00f323908142e3ba78d80210e056cf08ff
Rafli Hidayat <rafli170301maret@gmail.com>
2023-08-23 09:48:53 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+
+# Inisialisasi objek VideoCapture dengan indeks kamera (biasanya 0 untuk kamera utama)
+cap = cv2.VideoCapture(0)
+
+# Periksa apakah kamera berhasil diakses
+if not cap.isOpened():
+    print("Kamera tidak dapat diakses")
+    exit()
+
+while True:
+    # Baca frame dari kamera
+    ret, frame = cap.read()
+
+    # Periksa apakah frame berhasil dibaca
+    if not ret:
+        print("Tidak dapat membaca frame dari kamera")
+        break
+
+    # Tampilkan frame di jendela
+    cv2.imshow("Kamera", frame)
+
+    # Jika tombol 'q' ditekan, keluar dari loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Tutup kamera dan jendela tampilan
+cap.release()
+cv2.destroyAllWindows()
